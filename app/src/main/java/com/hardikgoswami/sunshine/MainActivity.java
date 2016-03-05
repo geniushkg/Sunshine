7af0e376d66f727b41bca2cd6e26f765f0cdd8fe
geniushkg <geniushkg@gmail.com>
2016-03-05 09:37:35 +0530

    Initial commit , Lesson 1 Added ListView

---
+++ app/src/main/java/com/hardikgoswami/sunshine/MainActivity.java
@@ -0,0 +1,42 @@
+package com.hardikgoswami.sunshine;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceHolderFragment());
+
+        }
+    }
+
+    public static class PlaceHolderFragment extends Fragment {
+
+        public PlaceHolderFragment() {
+
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container);
+            ArrayList<String> foreCastData = new ArrayList<String>();
+            foreCastData.add("today");
+
+            return rootView;
+        }
+    }
+}
