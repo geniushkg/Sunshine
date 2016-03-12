7503ac8ee7f59788fa9dda832de3de8f7e6793ca
geniushkg <geniushkg@gmail.com>
2016-03-12 09:55:51 +0530

    added arrayadapter

--- app/src/main/java/com/hardikgoswami/sunshine/MainActivity.java
+++ app/src/main/java/com/hardikgoswami/sunshine/MainActivity.java
@@ -4,14 +4,17 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,23 +22,31 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceHolderFragment());
-
+            Log.d("TAG","Main activity");
         }
     }
-
     public static class PlaceHolderFragment extends Fragment {
-
         public PlaceHolderFragment() {
-
         }
-
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container);
-            ArrayList<String> foreCastData = new ArrayList<String>();
-            foreCastData.add("today");
-
+            Log.d("TAG","placeholder called");
+            String[] foreCastArray = {"Today - Sunny 88/63 ", "Tommorow - Cloudy - 77/54",
+                    "Wednesday - Clear - 77/88", "Thursday- Sunny -66/44", "Friday- clear - 45/78",
+                    "Saturday - Clear - 45/54"};
+            Log.d("TAG","added to array");
+            ArrayList<String> foreCastData = new ArrayList<String>(Arrays.asList(foreCastArray));
+            ArrayAdapter<String> foreCastAdapter = new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textview,foreCastData);
+            ListView listViewForeCast = (ListView)rootView.findViewById(R.id.listview_forecast);
+            if(foreCastAdapter.isEmpty()){
+                Log.d("TAG","adapter empty");
+            }
+            listViewForeCast.setAdapter(foreCastAdapter);
+            if(listViewForeCast == null){
+                Log.d("TAG","Listview is null");
+            }
             return rootView;
         }
     }
