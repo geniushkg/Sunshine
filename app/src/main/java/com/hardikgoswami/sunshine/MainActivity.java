c5389d06670942edee416fee1ffae435e0843d0b
geniushkg <geniushkg@gmail.com>
2016-03-28 09:49:37 +0530

    adding network code

--- app/src/main/java/com/hardikgoswami/sunshine/MainActivity.java
+++ app/src/main/java/com/hardikgoswami/sunshine/MainActivity.java
@@ -10,6 +10,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -29,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         public PlaceHolderFragment() {
         }
         @Nullable
+
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container ,false);
@@ -39,6 +44,17 @@ public class MainActivity extends AppCompatActivity {
             ArrayAdapter<String> foreCastAdapter = new ArrayAdapter<>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textview,foreCastData);
             ListView listViewForeCast = (ListView)rootView.findViewById(R.id.listview_forecast);
             listViewForeCast.setAdapter(foreCastAdapter);
+
+            // declaring outside try/catch so that can be closed in finally
+            HttpURLConnection httpURLConnection = null;
+            BufferedReader reader = null;
+            String forecastJsonStr = null;
+            try{
+                // construct new url
+                URL url = new(http://openweathermap);
+            }
+
+
           return rootView;
         }
     }
