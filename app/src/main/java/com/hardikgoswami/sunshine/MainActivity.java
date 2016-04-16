2b2a4de6452402d9536368066a32714d45601d34
geniushkg <geniushkg@gmail.com>
2016-04-16 19:28:44 +0530

    Added Menu , AsyncTask

--- app/src/main/java/com/hardikgoswami/sunshine/MainActivity.java
+++ app/src/main/java/com/hardikgoswami/sunshine/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hardikgoswami.sunshine;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,7 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     public static String TAG ="SUNSHINE";
     @Override
@@ -22,29 +23,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        Log.d("TAG","main menu inflated");
-        return false;
+
+        return super.onCreateOptionsMenu(menu);
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        Toast.makeText(getApplicationContext(),"OnOptionMain",Toast.LENGTH_SHORT).show();
-        Log.d("TAG","main menu");
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Toast.makeText(getApplicationContext(),"OnOptionMain",Toast.LENGTH_SHORT).show();
-            return false;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 }
